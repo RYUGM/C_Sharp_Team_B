23a54357f8f6be45779f82d88a932eb67326d804
RYU <newruh2@gmail.com>
2022-01-20 09:18:00 +0900

    0120

---
+++ RYU-GM/0119/Parking_Manage/Parking_Manage/DataManager.cs
@@ -0,0 +1,190 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking_Manage
+{
+    public class DataManager
+    {
+        const string ORADB = "Data Source=(DESCRIPTION=(ADDRESS_LIST=" +
+               "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))" +
+               "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));" +
+               "User Id=c##scott;Password=tiger;";
+
+        //const string ORADB = "Data Source=(DESCRIPTION=(ADDRESS_LIST=" +
+        //       "(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))" +
+        //       "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));" +
+        //       "User Id=c##RYU;Password=newruh;";
+        public static OracleConnection OraConn = new OracleConnection(ORADB);
+
+        public static List<ParkingCar> cars = new List<ParkingCar>();
+        const string TABLE = "ParkingCar";
+
+        static void ConnectDB()
+        {
+            try
+            {
+                OraConn.Open();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("DB연결 애러"+ex.Message+
+                    "애러 위치"+ Environment.NewLine+ex.StackTrace);
+            }
+        }
+
+        public static void selectQuery()
+        {
+            ConnectDB();
+            string sql;
+            sql = "select * from " + TABLE + " order by to_number(parkingspot)";
+            OracleDataAdapter oda = new OracleDataAdapter();
+            oda.SelectCommand = new OracleCommand();
+            oda.SelectCommand.Connection = OraConn;
+            oda.SelectCommand.CommandText = sql;
+
+            DataSet ds = new DataSet();
+            oda.Fill(ds, TABLE);
+
+            cars.Clear();
+            foreach(DataRow item in ds.Tables[0].Rows)
+            {
+                ParkingCar car = new ParkingCar();
+                car.ParkingSpot = int.Parse(item["parkingspot"].ToString());
+                car.CarNumber = item["carnumber"].ToString();
+                car.DriverName = item["drivername"].ToString();
+                car.PhoneNumber = item["phonenumber"].ToString();
+                car.ParkingTime = item["parkingtime"].ToString() ==
+                    "" ? new DateTime() : DateTime.Parse(item["parkingtime"].ToString());
+                car.result1 = (item["result1"].ToString())+"원";
+
+                cars.Add(car);
+            }
+            OraConn.Close();
+        }
+
+
+        public static ParkingCar selectQuery(int spot)
+        {
+            ConnectDB();
+            string sql;
+            sql = "select * from " + TABLE + " where parkingspot="+ spot +" order by to_number(parkingspot)";
+            OracleDataAdapter oda = new OracleDataAdapter();
+            oda.SelectCommand = new OracleCommand();
+            oda.SelectCommand.Connection = OraConn;
+            oda.SelectCommand.CommandText = sql;
+
+            DataSet ds = new DataSet();
+            oda.Fill(ds, TABLE);
+
+            //cars.Clear();
+
+            //foreach (DataRow item in ds.Tables[0].Rows)
+            //{
+                DataRow item = ds.Tables[0].Rows[0];
+                ParkingCar car = new ParkingCar();
+                car.ParkingSpot = int.Parse(item["parkingspot"].ToString());
+                car.CarNumber = item["Carnumber"].ToString();
+                car.DriverName = item["Drivername"].ToString();
+                car.PhoneNumber = item["Phonenumber"].ToString();
+                car.ParkingTime = item["Parkingtime"].ToString() ==
+                    "" ? new DateTime() : DateTime.Parse(item["parkingtime"].ToString());
+            car.result1 = item["result1"].ToString();
+
+                //cars.Add(car);
+            //}
+            OraConn.Close();
+            return car;
+        }
+
+        static string Query(string menu,string parkingspot,string carnumber,string drivername, string phonenumber)
+        {
+            string query = "";
+            switch (menu)
+            {
+                case "update":
+                    query = $"update {TABLE} set " +
+                        $"carnumber='{carnumber}',drivername='{drivername}'," +
+                        $"phonenumber='{phonenumber}',parkingtime=sysdate,result = to_char(systimestamp,'HH24') " +
+                        $" where parkingspot={parkingspot}";
+                    break;
+                case "insert":
+                    query = $"insert into {TABLE} (parkingspot) values({parkingspot})";
+                    break;
+                case "delete":
+                    query = $"delete from {TABLE} where parkingspot = {parkingspot}";
+                    break;
+                
+                default:
+                    break;
+            }
+            return query;
+        }
+        static string Query(string menu, string parkingspot)
+        {
+            //여기서 DB 내부적으로 정산버튼을 누른 시간 - 주차 시작한시간 에 곱하기 1000을 해서 시간당 천원이 나옴
+            string query = $"update {TABLE} set result1 = (select sum(((to_char(systimestamp,'HH24')+1)-result)*1000) from {TABLE} where parkingspot={parkingspot}) where parkingspot={parkingspot}";
+          
+            return query;
+        }
+        public static void executeQuery(string menu, string parkingspot, string carnumber=null, string drivername=null, string phonenumber=null)
+        {
+            ConnectDB();
+            string query = "";
+
+            try
+            {
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = OraConn;
+                query = Query(menu, parkingspot, carnumber, drivername, phonenumber);
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                OraConn.Close();
+                throw new Exception(query+"_"+ ex.Message+"~~"+Environment.NewLine+ex.StackTrace);
+            }
+
+            OraConn.Close();
+
+            selectQuery();
+        }
+        public static void executeQuery1(string menu, string parkingspot)
+        {
+            
+            ConnectDB();
+            string query = "";
+
+            try
+            {
+               
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = OraConn;
+                query = Query(menu, parkingspot);
+                //update와 주차 번호를 Query에 담고 실행
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                OraConn.Close();
+                throw new Exception(query + "_" + ex.Message + "~~" + Environment.NewLine + ex.StackTrace);
+            }
+
+            OraConn.Close();
+
+            selectQuery();
+        }
+
+
+    }
+
+}
---
+++ RYU-GM/0119/Parking_Manage/Parking_Manage/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parking_Manage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+            DataManager.selectQuery();
+            refreshScreen();
+        }
+
+        void refreshScreen()
+        {
+            dataGridView_Parking_Car_View.DataSource = null;
+            //dataGridView_Parking_Car_View1.DataSource = null;
+            try
+            {
+                if(DataManager.cars.Count > 0)
+                {
+                    dataGridView_Parking_Car_View.DataSource = DataManager.cars;
+                    //dataGridView_Parking_Car_View1.DataSource = DataManager.cars;
+                    textBox_parking_spot.Text = DataManager.cars[0].ParkingSpot.ToString();
+                    textBox_spot_manager.Text = textBox_parking_spot.Text;
+                    textBox_carnum.Text = DataManager.cars[0].CarNumber;
+                    textBox_driver_name.Text = DataManager.cars[0].DriverName;
+                    textBox_phone_num.Text = DataManager.cars[0].PhoneNumber;
+                    
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_park_in_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox_parking_spot.Text == "")
+                {
+                    MessageBox.Show("주차 공간 입력 하세요");
+                    return;
+                }
+                if(textBox_carnum.Text == "")
+                {
+                    MessageBox.Show("차번호 입력");
+                    return;
+                }
+                ParkingCar car = DataManager.selectQuery(int.Parse(textBox_parking_spot.Text));
+                if(car.CarNumber != "")
+                {
+                    MessageBox.Show("이미 주차 됨");
+                }
+                else
+                {
+                    DataManager.executeQuery("update",textBox_parking_spot.Text,textBox_carnum.Text,
+                        textBox_driver_name.Text,textBox_phone_num.Text);
+                    DataManager.selectQuery();
+                    refreshScreen();
+                    //dataGridView_Parking_Car_View.DataSource = null;
+                    //dataGridView_Parking_Car_View.DataSource = DataManager.cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
+                throw;
+            }
+        }
+
+        private void btn_park_out_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox_parking_spot.Text == "")
+                {
+                    MessageBox.Show("주차 공간 입력 하세요");
+                    return;
+                }
+               
+                ParkingCar car = DataManager.selectQuery(int.Parse(textBox_parking_spot.Text));
+                if (car.CarNumber == "")
+                {
+                    MessageBox.Show("아직 차가 없습니다.");
+                }
+                else
+                {
+                    DataManager.executeQuery("update", textBox_parking_spot.Text, "", "", "");
+                    DataManager.selectQuery();
+                    refreshScreen();
+                    //dataGridView_Parking_Car_View.DataSource = null;
+                    //dataGridView_Parking_Car_View.DataSource = DataManager.cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
+                throw;
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            DataManager.executeQuery("insert", textBox_spot_manager.Text);
+            refreshScreen();
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            DataManager.executeQuery("delete", textBox_spot_manager.Text);
+            refreshScreen();
+        }
+
+        private void dataGridView_Parking_Car_View_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ParkingCar car = dataGridView_Parking_Car_View.CurrentRow.DataBoundItem as ParkingCar;
+            textBox_spot_manager.Text = textBox_parking_spot.Text;
+
+            textBox_carnum.Text = car.CarNumber;
+            textBox_driver_name.Text = car.DriverName;
+            textBox_phone_num.Text = car.PhoneNumber;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+           Close();
+           
+        }
+
+        private void textBox_spot_manager_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_Parking_Car_View_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void f1223qawed_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        //private void dataGridView_Parking_Car_View1_CellClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    ParkingCar car = dataGridView_Parking_Car_View1.CurrentRow.DataBoundItem as ParkingCar;
+        //    textBox_spot_manager.Text = textBox_parking_spot.Text;
+
+        //    textBox_carnum.Text = car.CarNumber;
+        //    textBox_driver_name.Text = car.DriverName;
+        //    textBox_phone_num.Text = car.PhoneNumber;
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void uiButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiLabel8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiTextBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void qwe123_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void uiButton2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //시간당 천원 , 정산 버튼 
+            try
+            {
+                if (textBox_parking_spot.Text == "")
+                {
+                    MessageBox.Show("주차 공간 입력 하세요");
+                    //정산할 공간을 입력 안할시 출력
+                    return;
+                }
+
+             
+                else
+                {
+                    //update와 주차 공간 값을 가지고 executeQuery1로 이동
+                    DataManager.executeQuery1("update", textBox_parking_spot.Text);
+
+                    DataManager.selectQuery();
+                    refreshScreen();
+                    //dataGridView_Parking_Car_View.DataSource = null;
+                    //dataGridView_Parking_Car_View.DataSource = DataManager.cars;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
+                throw;
+            }
+        }
+    }
+}
